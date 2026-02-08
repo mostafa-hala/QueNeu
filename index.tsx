75d28753dbfcc78c5c06d4add5d16ae9c53dbbba
mostafahala <94720969+mostafa-hala@users.noreply.github.com>
2026-02-08 17:21:07 +0200

    Use HashRouter for GitHub Pages

--- index.tsx
+++ index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element to mount to");
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
