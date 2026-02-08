6f168cd3421fc6f304550cd60682cdd9a5dd00cc
mostafahala <94720969+mostafa-hala@users.noreply.github.com>
2026-02-08 18:14:13 +0200

    Deploy Vite to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/QueNeu/',
+  base: '/QueNeu/',   // 👈 اسم الريبو بالظبط
   plugins: [react()],
 })
